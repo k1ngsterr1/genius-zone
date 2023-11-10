0529e79affe01f5710e2035eafa8aafd3cdff507
Ruslan Makhmatov <ruslanmakhmatov@gmail.com>
2023-11-10 18:20:32 +0600

    Version 1.1.0

--- src/features/header/ui/index.tsx
+++ src/features/header/ui/index.tsx
@@ -1,15 +1,23 @@
 import { Logo } from "@shared/index";
 import { Link } from "react-router-dom";
 
+import "./styles.scss";
+
 export const Header = () => {
   return (
     <header className="header flex items-center justify-between">
-      <div className="header__content mt-8 flex items-center justify-between">
+      <div className="header__container flex items-center justify-between mt-8">
         <Logo />
-        <div className="header__links">
-          <Link to="/">Главная</Link>
-          <Link to="/study">Обучение</Link>
-          <Link to="/gift">Преподование</Link>
+        <div className="header__links flex items-center justify-between">
+          <Link to="/" className="link">
+            Главная
+          </Link>
+          <Link to="/study" className="link">
+            Обучение
+          </Link>
+          <Link to="/gift" className="link">
+            Преподование
+          </Link>
         </div>
       </div>
     </header>
