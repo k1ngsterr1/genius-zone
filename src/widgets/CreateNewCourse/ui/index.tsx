e0a0fe2297e4793b69b2d0adeccc6553db8f4010
Ruslan Makhmatov <ruslanmakhmatov@gmail.com>
2023-12-02 20:39:07 +0600

    Version 1.5.7

--- src/widgets/CreateNewCourse/ui/index.tsx
+++ src/widgets/CreateNewCourse/ui/index.tsx
@@ -1,22 +1,47 @@
+import { useState, FormEvent } from "react";
 import { CreateCourseSide } from "@features/SidePanels/CreateCourse/ui";
 import { TextField } from "@mui/material";
 import { Button } from "@shared/ui/button";
+import { useCreateCourse } from "@widgets/form/lib/useCreateCourse";
 
 import "./styles.scss";
 
 export const CreateNewCourseScreen = () => {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const { createCourse } = useCreateCourse();
+
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault(); // Prevent the default form submission
+    await createCourse({ title, description }); // Call createCourse with the form data
+  };
+
   return (
     <div className="wrapper--row mb-12">
       <CreateCourseSide />
       <section className="w-[73%]  new-courses-container flex flex-col">
         <h1 className="main-heading">Создание нового курса</h1>
-        <form className="flex flex-col">
+        <form className="flex flex-col" onSubmit={handleSubmit}>
           <TextField
             required
+            name="title"
             variant="outlined"
             placeholder="Введите название вашего курса*"
             type="text"
             className="new-courses-container__input mt-8"
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <TextField
+            required
+            name="description"
+            variant="outlined"
+            multiline
+            maxRows={4}
+            size={"medium"}
+            placeholder="Введите описание вашего курса*"
+            type="text"
+            className="new-courses-container__input mt-8"
+            onChange={(e) => setDescription(e.target.value)}
           />
           <Button
             text="Создать курс"
---
+++ src/widgets/form/lib/useCreateCourse.ts
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+export interface CourseData {
+  title: string;
+  description: string;
+}
+
+export function useCreateCourse() {
+  const createCourse = async (data: CourseData) => {
+    // event?.preventDefault();
+    try {
+      console.log("trying");
+      const response = await axios.post(
+        `${import.meta.env.VITE_BASE_URL}api/courses/courses`,
+        data
+      );
+      console.log("Course Created Successfully:", response.data);
+    } catch (error: any) {
+      console.error("There is an error with course creating:", error.response);
+    }
+  };
+
+  return {
+    createCourse,
+  };
+}
