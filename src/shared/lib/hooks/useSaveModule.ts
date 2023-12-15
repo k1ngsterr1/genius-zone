f14d792afaf97d3acdb11c2fb2dbb2fd2b771e08
Ruslan Makhmatov <ruslanmakhmatov@gmail.com>
2023-12-15 15:59:36 +0600

    Version 1.7.9

--- src/shared/lib/hooks/useSaveModule.ts
+++ src/shared/lib/hooks/useSaveModule.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import Cookies from "js-cookie";
 
 export interface ModuleData {
   module_title: string;
@@ -8,9 +9,15 @@ export interface ModuleData {
 export const useSaveModule = () => {
   const saveModule = async (data: ModuleData, courseID: string | any) => {
     try {
+      const token = Cookies.get("accessToken");
       const response = await axios.post(
         `https://inquisitive-creature-production.up.railway.app/api/courses/course/${courseID}/module/`,
-        data
+        data,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
       );
       console.log("Module Saved!", response.data);
     } catch (error: any) {
