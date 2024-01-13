6fa1c22e86f74ca4de0abc021ccd196110b4f364
Ruslan Makhmatov <ruslanmakhmatov@gmail.com>
2024-01-13 21:26:55 +0600

    Version 2.4.7

---
+++ src/shared/lib/hooks/useStartConversation.ts
@@ -0,0 +1,26 @@
+import axios from "axios";
+import Cookies from "js-cookie";
+
+function useStartConversation() {
+  const token = Cookies.get("accessToken");
+
+  async function startConversation() {
+    try {
+      const response = await axios.post(
+        "https://genzone.up.railway.app/api/conversations/start/",
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      console.log("Conversation successfully started", response.data);
+    } catch (error: any) {
+      console.error("There was an error with conversation start:", error);
+    }
+  }
+
+  return { startConversation };
+}
+
+export default useStartConversation;
--- src/widgets/Chats/ui/index.tsx
+++ src/widgets/Chats/ui/index.tsx
@@ -4,16 +4,32 @@ import { ChatBar } from "@widgets/ChatBar/ui";
 import { faCheck, faCheckDouble } from "@fortawesome/free-solid-svg-icons";
 import useLoadConversations from "@shared/lib/hooks/useLoadConversations";
 
+import noChats from "@assets/no_courses.svg";
+
 export const ChatsScreen = () => {
   const { conversations } = useLoadConversations();
   const [isRead, setIsRead] = useState(false);
+
+  if (!conversations) {
+    return (
+      <div className="wrapper--row mb-12">
+        <UserAside />
+        <section className="w-[73%] new-courses-container flex flex-col max-[640px]:hidden">
+          <div className="flex flex-col justify-center items-center h-full">
+            <img src={noChats} className="no-courses" alt="no-courses-vector" />
+            <h1 className="text-3xl text-custom-black">У вас пока нет чатов</h1>
+          </div>
+        </section>
+      </div>
+    );
+  }
 
   return (
     <div className="wrapper--row mb-12">
       <UserAside />
       <section className="w-[73%] new-courses-container flex flex-col max-[640px]:hidden">
         <h1 className="main-heading">Чаты</h1>
-        {conversations.map((conversation) => {
+        {conversations.map((conversation: any) => {
           const initiator = conversation.initiator;
           return (
             <ChatBar
