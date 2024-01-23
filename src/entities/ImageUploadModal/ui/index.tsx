75c319cf5cb3bd55e284229b044224ef651928d8
Chechen Daddy <ruslanmakhmatov@gmail.com>
2024-01-23 15:23:03 +0600

    Version 2.8.1

--- src/entities/ImageUploadModal/ui/index.tsx
+++ src/entities/ImageUploadModal/ui/index.tsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import { faClose, faPaperPlane } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+import "./styles.scss";
+
 interface ImageUploadProps {
   image: any;
   onSend: () => void;
@@ -16,34 +18,38 @@ export const ImageUploadModal: React.FC<ImageUploadProps> = ({
   const [caption, setCaption] = useState("");
 
   return (
-    <div className="image-upload-modal">
-      <div className="image-upload-modal__content">
-        <div className="image-upload-modal__content__upper">
-          <FontAwesomeIcon
-            icon={faClose}
-            onClick={onClose}
-            className="image-upload-modal__content__upper__icon"
+    <div className="background_black">
+      <div className="image-upload-modal">
+        <div className="image-upload-modal__content">
+          <div className="image-upload-modal__content__upper">
+            <FontAwesomeIcon
+              icon={faClose}
+              onClick={onClose}
+              className="image-upload-modal__content__upper__icon"
+            />
+            <span className="image-upload-modal__content__upper__text">
+              Отправить фото
+            </span>
+          </div>
+          <img
+            className="image-upload-modal__content__preview"
+            src={URL.createObjectURL(image)}
+            alt="Preview"
           />
-          <span className="image-upload-modal__content__upper__text"></span>
-        </div>
-        <img
-          className="image-upload-modal__content__preview"
-          src={URL.createObjectURL(image)}
-          alt="Preview"
-        />
-        <div className="image-upload-modal__content__lower">
-          <input
-            type="text"
-            placeholder="Add a caption..."
-            value={caption}
-            onChange={(e) => setCaption(e.target.value)}
-            className="image-upload-modal__content__lower__input"
-          />
-          <FontAwesomeIcon
-            icon={faPaperPlane}
-            onClick={onSend}
-            className="image-upload-modal__content__lower__button"
-          />
+          <div className="image-upload-modal__content__lower">
+            <input
+              type="text"
+              placeholder="Add a caption..."
+              value={caption}
+              onChange={(e) => setCaption(e.target.value)}
+              className="image-upload-modal__content__lower__input"
+            />
+            <FontAwesomeIcon
+              icon={faPaperPlane}
+              onClick={onSend}
+              className="image-upload-modal__content__lower__button"
+            />
+          </div>
         </div>
       </div>
     </div>
